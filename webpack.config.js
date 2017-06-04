252c0ebad8b082a7fcbf98e137ba04dfd89e66cb
Regis Biron <regis.biron@upstatement.com>
2017-06-04 13:45:23 -0400

    convert to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -6,55 +6,55 @@ const postcss = require('postcss');
 const port = 3000;
 
 module.exports = {
-	context: __dirname,
-	devtool: 'inline-source-map',
-	entry: [
-	'react-hot-loader/patch',
-	`webpack-dev-server/client?http://localhost:${port}`,
-	'webpack/hot/only-dev-server',
-	path.join(__dirname, 'src', 'index.js'),
-	],
-	output: {
-		path: __dirname,
-		filename: '[name].bundle.js',
-		publicPath: `http://localhost:${port}/`
-	},
-	devServer: {
-		hot: true,
-		open: true,
-		inline: true,
-		port: port,
-		publicPath: '/',
-		historyApiFallback: true,
-		contentBase: __dirname,
-	},
-	module: {
-		rules: [
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: ['babel-loader']
-		},
-		{
-			test: /\.css$/,
-			use: [ 'style-loader',
-			{
-				loader: 'css-loader',
-				options: {
-					modules: true,
-					localIdentName: '[name]__[local]___[hash:base64:5]'
-				}
-			}, {
-				loader: 'postcss-loader',
-				options: {
-					config: {
-						path: path.join(__dirname, './postcss.config.js')
-					}
-				}
-			}]
-		}]
-	},
-	plugins: [
-	new webpack.HotModuleReplacementPlugin()
-	]
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: [
+  'react-hot-loader/patch',
+  `webpack-dev-server/client?http://localhost:${port}`,
+  'webpack/hot/only-dev-server',
+  path.join(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js',
+    publicPath: `http://localhost:${port}/`
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    inline: true,
+    port: port,
+    publicPath: '/',
+    historyApiFallback: true,
+    contentBase: __dirname,
+  },
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    {
+      test: /\.css$/,
+      use: [ 'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          config: {
+            path: path.join(__dirname, './postcss.config.js')
+          }
+        }
+      }]
+    }]
+  },
+  plugins: [
+  new webpack.HotModuleReplacementPlugin()
+  ]
 }
