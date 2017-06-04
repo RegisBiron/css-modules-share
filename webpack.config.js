93db6e1e1fdb0e042a59de10bc1b1ee28be7b935
Regis Biron <regis.biron@upstatement.com>
2017-06-04 13:42:00 -0400

    indent

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,10 @@ module.exports = {
 	context: __dirname,
 	devtool: 'inline-source-map',
 	entry: [
-		'react-hot-loader/patch',
-		`webpack-dev-server/client?http://localhost:${port}`,
-		'webpack/hot/only-dev-server',
-		path.join(__dirname, 'src', 'index.js'),
+	'react-hot-loader/patch',
+	`webpack-dev-server/client?http://localhost:${port}`,
+	'webpack/hot/only-dev-server',
+	path.join(__dirname, 'src', 'index.js'),
 	],
 	output: {
 		path: __dirname,
@@ -20,41 +20,41 @@ module.exports = {
 		publicPath: `http://localhost:${port}/`
 	},
 	devServer: {
-    hot: true,
-    open: true,
-    inline: true,
-    port: port,
-    publicPath: '/',
-    historyApiFallback: true,
-    contentBase: __dirname,
-  },
+		hot: true,
+		open: true,
+		inline: true,
+		port: port,
+		publicPath: '/',
+		historyApiFallback: true,
+		contentBase: __dirname,
+	},
 	module: {
 		rules: [
 		{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    },
-    {
-		 test: /\.css$/,
-		 use: [ 'style-loader',
-		   {
-		     loader: 'css-loader',
-		     options: {
-		       modules: true,
-		       localIdentName: '[name]__[local]___[hash:base64:5]'
-		     }
-		   }, {
-		   	loader: 'postcss-loader',
-		   	options: {
-		   		config: {
-		   			path: path.join(__dirname, './postcss.config.js')
-		   		}
-		    }
-		  }]
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: ['babel-loader']
+		},
+		{
+			test: /\.css$/,
+			use: [ 'style-loader',
+			{
+				loader: 'css-loader',
+				options: {
+					modules: true,
+					localIdentName: '[name]__[local]___[hash:base64:5]'
+				}
+			}, {
+				loader: 'postcss-loader',
+				options: {
+					config: {
+						path: path.join(__dirname, './postcss.config.js')
+					}
+				}
+			}]
 		}]
 	},
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+	plugins: [
+	new webpack.HotModuleReplacementPlugin()
+	]
 }
