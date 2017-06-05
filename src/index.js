02306bb45e297fb0f816cbc43750317d02f88d8a
Regis Biron <regis.biron@upstatement.com>
2017-06-04 22:09:41 -0400

    restructred files

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {
 } from 'react-router-dom';
 
 import App from './views/App/App';
-import Home from './components/Home/Home';
-import About from './components/About/About';
+import Home from './views/Home/Home';
+import About from './views/About/About';
 
 render((
 	<Router>
