203c36f7e7adb12e69fd1b7e7c6e893c9f078ff2
Regis Biron <regis.biron@upstatement.com>
2017-06-16 15:31:32 -0400

    postcss modules values

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-modules-values'),
     require('postcss-cssnext'),
     require('postcss-short'),
     require('postcss-nested')
